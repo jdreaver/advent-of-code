3cf7e87e45e6d3d2ea5f9a8e15d7274d58a962cc
David Reaver <johndreaver@gmail.com>
2021-11-26 09:12:05 -0800

    2020 13 part 2

--- src/bin/2020-13-shuttle-search.rs
+++ src/bin/2020-13-shuttle-search.rs
@@ -1,6 +1,7 @@
 fn main() {
-    let input = parse_input(_EXAMPLE);
+    let input = parse_input(INPUT);
     println!("part1: {}", first_bus(&input));
+    println!("part2: {}", first_consecutive_depart_timestamp(&input.bus_ids));
 }
 
 fn first_bus(input: &Input) -> u32 {
@@ -12,6 +13,24 @@ fn first_bus(input: &Input) -> u32 {
         .min()
         .expect("couldn't find first bus");
     min_wait * id
+}
+
+fn first_consecutive_depart_timestamp(bus_ids: &[Option<u32>]) -> u64 {
+    let ids_offsets = bus_ids
+        .iter()
+        .enumerate()
+        .filter_map(|(offset, mid)| mid.map(|id| (offset, id as u64)))
+        .collect::<Vec<(usize, u64)>>();
+
+    let mut step_size = ids_offsets[0].1;
+    let mut t = 0;
+    for (offset, id) in ids_offsets.iter().skip(1) {
+        while (t + *offset as u64).rem_euclid(*id) != 0 {
+            t += step_size;
+        }
+        step_size *= id;
+    }
+    t
 }
 
 #[derive(Debug)]
@@ -36,3 +55,6 @@ fn parse_input(input: &str) -> Input {
 
 const _EXAMPLE: &str = "939
 7,13,x,x,59,x,31,19";
+
+const INPUT: &str = "1006697
+13,x,x,41,x,x,x,x,x,x,x,x,x,641,x,x,x,x,x,x,x,x,x,x,x,19,x,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,661,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23";
