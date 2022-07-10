676d03b9e5af8b76b417d5fddbcd2f1ddde099ee
David Reaver <me@davidreaver.com>
2022-07-10 09:02:07 -0700

    Move flake to subdirectory

--- shell.nix
+++ shell.nix
@@ -1,3 +1,8 @@
 { system ? builtins.currentSystem }:
 
-(builtins.getFlake (toString ./.)).devShell.${system}
+# Flake is in a subdirectory so we don't copy this entire repo to /nix/store.
+# See:
+# - https://github.com/NixOS/nix/issues/3121
+# - https://discourse.nixos.org/t/tweag-nix-dev-update-31/19481
+# - https://github.com/NixOS/nix/issues/4097
+(builtins.getFlake (toString ./flake)).devShell.${system}
